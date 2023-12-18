afacd7ae839d635b59d775c9e8eaa38400383821
Oleg Mitnik <olegmitnik.dev@gmail.com>
2023-12-18 12:49:34 +0300

    Delete saved_gist from user/gist if user/gist have been deleted

--- priv/repo/migrations/20231217210854_create_saved_gists.exs
+++ priv/repo/migrations/20231217210854_create_saved_gists.exs
@@ -4,8 +4,8 @@ defmodule ElixirGist.Repo.Migrations.CreateSavedGists do
   def change do
     create table(:saved_gists, primary_key: false) do
       add :id, :binary_id, primary_key: true
-      add :user_id, references(:users, on_delete: :nothing, type: :binary_id)
-      add :gist_id, references(:gists, on_delete: :nothing, type: :binary_id)
+      add :user_id, references(:users, on_delete: :delete_all, type: :binary_id)
+      add :gist_id, references(:gists, on_delete: :delete_all, type: :binary_id)
 
       timestamps(type: :utc_datetime)
     end
